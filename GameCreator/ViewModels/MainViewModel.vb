73fef1429c897995cf2d6bb083e904da1bd413ac
JBou <gabriel@welsberg.it>
2014-09-18 15:31:18 +0200

    Added Basic Stuff

---
+++ GameCreator/ViewModels/MainViewModel.vb
@@ -0,0 +1,20 @@
+﻿Public Class MainViewModel
+
+#Region "Singleton & Constructor"
+    Private Shared _Instance As MainViewModel
+    Public Shared ReadOnly Property Instance As MainViewModel
+        Get
+            If _Instance Is Nothing Then _Instance = New MainViewModel
+            Return _Instance
+        End Get
+    End Property
+
+    Public Sub New()
+    End Sub
+
+#End Region
+
+#Region "Properties"
+
+#End Region
+End Class
